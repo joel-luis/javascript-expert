afa9094606406b34df38bc1cf67d8c337a4ca8e0
Joel Luis <joelluis73@gmail.com>
2023-10-26 21:58:51 -0300

    Aula 2

---
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,55 @@
+import CanvasRenderer from "./canvasRenderer.js"
+import MP4Demuxer from "./mp4Demuxer.js"
+import VideoProcessor from "./videoProcessor.js"
+
+const qvgaConstraints = {
+  with: 320,
+  height: 240
+}
+
+const vgaConstraints = {
+  with: 640,
+  height: 480
+}
+
+const hdConstraints = {
+  with: 1280,
+  height: 720
+}
+
+const encoderConfig = {
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  // WebM
+  codec: 'vp09.00.10.08',
+  pt: 4,
+  hardwareAcceleration: 'prefer-software',
+
+  //MP4
+  // codec: 'avc1.42002A',
+  // pt: 1,
+  // hardwareAcceleration: 'prefer-hardware',
+  // avc: {format: 'annexb' }
+}
+
+const mp4Demuxer = new MP4Demuxer()
+const videoProcessor = new VideoProcessor({
+  mp4Demuxer
+})
+
+
+onmessage = async ({ data }) => {
+  const renderFrame = CanvasRenderer.getRenderer(data.canvas)
+  await videoProcessor.start({
+    file: data.file,
+    renderFrame,
+    encoderConfig,
+    sendMessage(message) {
+      self.postMessage(message)
+    }
+  })
+
+  self.postMessage({
+    status: 'done'
+  })
+}
